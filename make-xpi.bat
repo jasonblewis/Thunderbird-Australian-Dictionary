9b0596dc50edff7c5b31b412446489f787bb5735
unknown <jason@cyan.(none)>
2012-01-19 15:33:59 +1100

    updated for TB9 and FF9 changed path to 7zip for 64bit path

--- make-xpi.bat
+++ make-xpi.bat
@@ -1,1 +1,1 @@
-"c:\Program Files (x86)\7-Zip\7z.exe" a -r -y "en-au-dictionary.xpi"  changelog.txt dictionaries install.js install.rdf license.txt readme.txt 
+"c:\Program Files\7-Zip\7z.exe" a -r -y "en-au-dictionary.xpi"  changelog.txt dictionaries install.js install.rdf license.txt readme.txt 
